642cbaaa665a21d56dd0dfe27daa7885513b5f38
Adzi <belkoadam@gmail.com>
2022-09-29 20:27:16 +1000

    DOM rendering for both players functional

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 
+const player = (mark) => {
+    getMark = () => mark;
+    return {getMark};
+};
+
 const gameBoard = (() => {
-    const board = ["O", "O", "", "X", "O", "", "", "X", ""];
-    return {board};
+    const board = ["", "", "", "", "", "", "", "", ""];
+    const updateBoard = (fieldIndex, mark) => {
+        board[fieldIndex] = mark;
+    }
+
+    return {board, updateBoard};
 })();
 
 const displayController = (() => {
-    const boardField = document.querySelectorAll(".gameboard-field");
+    const message = document.querySelector(".message");
+    const fieldEl = document.querySelectorAll(".gameboard-field");
     const restartBtn = document.querySelector(".restart-btn");
 
-    boardField.forEach(field => {
-        // field.addEventListener("click", e => {
-        field.textContent = gameBoard.board[field.dataset.index];
-        // })
+    fieldEl.forEach((field) => {
+        field.addEventListener("click", () => {
+            fieldIndex = field.dataset.index;
+            gameController.playRound(fieldIndex);
+            field.textContent = gameBoard.board[fieldIndex];
+        });
     })
 })();
+
+const gameController = (() => {
+    const playerX = player("X");
+    const playerO = player("O");
+    let round = 1;
+
+    const playRound = (fieldIndex) => {
+        (round % 2 === 0) ? gameBoard.updateBoard(fieldIndex, playerO.getMark()) : gameBoard.updateBoard(fieldIndex, playerX.getMark());
+        round++;
+    }
+
+    return {playRound};
+})();
+
+
+
+// gameBoard.board.splice(fieldIndex, 1, playerX.getMark());
+// console.log(gameBoard.board);
+// fieldIndex.textContent = gameBoard.board[fieldIndex];
+// round++;
+
+// const updateBoard = (fieldIndex, player) => {
+//     return update = () => gameBoard.board.splice(fieldIndex, 1, player);
+// };
