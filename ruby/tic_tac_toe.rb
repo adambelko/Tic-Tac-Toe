f4d76ad7e533489016e6dae0132aa588875fca9e
Adzi <belkoadam@gmail.com>
2023-06-20 20:02:54 +1000

    Progressed with game flow, game now takes an input, check validity and if valid proceedes to insert marker and show it on a  gameboard, keep looping till gameboard is full

--- ruby/tic_tac_toe.rb
+++ ruby/tic_tac_toe.rb
@@ -11,21 +11,41 @@ def display_board(board)
   puts
 end
 
-def play(board)
+def input_to_index
   puts "Insert number from 1 to 9"
-  index = gets.chomp
-  index = value_to_index(index)
-  return unless valid_move?(board, index)
+  value = gets.chomp
+  value_to_index(value)
+end
 
-  insert_marker(board, index)
+def play(board)
+  counter = 1
+
+  while counter <= 9
+    index = input_to_index
+    index = input_to_index until valid_move?(board, index)
+
+    if counter.odd?
+      insert_marker("X", board, index)
+    else
+      insert_marker("O", board, index)
+    end
+
+    counter += 1
+  end
 end
 
 def valid_move?(board, index)
-  if valid_range?(index + 1) && !position_taken?(board, index - 1)
-    true
-  else
-    puts "Invalid index"
+  unless valid_range?(index + 1)
+    puts "Invalid number / Not a number"
+    return false
   end
+
+  if position_taken?(board, index)
+    puts "Position already taken!"
+    return false
+  end
+
+  true
 end
 
 def valid_range?(index)
@@ -33,15 +53,16 @@ def valid_range?(index)
 end
 
 def position_taken?(board, index)
-  board[index] == "X" || board[index] == "Y"
+  board[index] == "X" || board[index] == "O"
 end
 
 def value_to_index(value)
   value.to_i - 1
 end
 
-def insert_marker(board, index)
-  board[index] = "X"
+def insert_marker(player, board, index)
+  p board
+  board[index] = player
   display_board(board)
 end
 
